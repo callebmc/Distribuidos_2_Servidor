d2aecb00069da3c0a6853d5892452c27d97b47e1
Calleb Malinoski <calleb.malinoski@gmail.com>
2019-10-22 18:48:36 -0300

    Adiciona método para inserir Aluno/Curriculo

--- Dist2_Servidor/src/dist2_interface/ServImpl.java
+++ Dist2_Servidor/src/dist2_interface/ServImpl.java
@@ -7,6 +7,7 @@ package dist2_interface;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 
 /**
  *
@@ -14,10 +15,27 @@ import java.rmi.server.UnicastRemoteObject;
  */
 public class ServImpl extends UnicastRemoteObject implements InterfaceServ{
 
-    public ServImpl() throws RemoteException{}
+    private ArrayList<Aluno> alunosCadastrados;
+    private ArrayList<Empresa> vagasCadastradas;    
+    
+    public ServImpl() throws RemoteException{
+        this.alunosCadastrados = new ArrayList<>();
+        this.vagasCadastradas = new ArrayList<>();
+    }
     
     @Override
     public void chamar(String mensagem, InterfaceCli referenciaCli) throws RemoteException {
         referenciaCli.echo(mensagem);
+    }
+    
+    @Override
+    synchronized public boolean inserirAluno(Aluno a){
+        if(this.alunosCadastrados.contains(a)){
+            System.out.println("Aluno já cadastrado no sistema");
+            return false;
+        }
+        this.alunosCadastrados.add(a);
+        System.out.println("Aluno cadastrado no sistema");
+        return true;
     }
 }
